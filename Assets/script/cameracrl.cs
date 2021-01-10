bd96304054b67b3454687d33c84a9df4a5561a4a
Junghs <rabbit4935@gmail.com>
2021-01-11 00:20:22 +0900

    w1

---
+++ Assets/script/cameracrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracrl : MonoBehaviour
+{
+
+    public Transform target;
+
+    private Vector3 came;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        came = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, came.z);
+    }
+}
--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -6,11 +6,12 @@ public class player : MonoBehaviour
 {
     Vector3 position;
 
-    public float speed = 2f;
+    public float speed = 2.25f;
     public float jump_force = 5f;
 
     public SpriteRenderer rend;
     Rigidbody2D rigid;
+    public Animator animator;
     public bool is_jumping=false;
 
 
@@ -20,11 +21,18 @@ public class player : MonoBehaviour
 
         rend = GetComponent<SpriteRenderer>();
         rigid = gameObject.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         is_jumping = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject,0.1f);
+        Debug.Log("체리");
     }
 
     // Update is called once per frame
@@ -38,11 +46,17 @@ public class player : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
             rend.flipX = true;
+            animator.SetBool("walk", true);
         }
 
         else if (Input.GetAxisRaw("Horizontal") > 0)
         {
             rend.flipX = false;
+            animator.SetBool("walk", true);
+        }
+        else
+        {
+            animator.SetBool("walk", false);
         }
 
         if (is_jumping ==false)
